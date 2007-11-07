d5c36ec7729072053bad58c54f35db15df9fe3fc
Jake Wires <jwires@xensource.com>
2007-11-06 20:46:55 -0800

    update td-util fill to work with VHD files created without block pre-allocation Signed-off-by: Jake Wires <jwires@xensource.com>

--- drivers/vhd-fill.c
+++ drivers/vhd-fill.c
@@ -24,11 +24,16 @@
 #define SECTOR_SHIFT         9
 
 struct vhd_context {
+	int                  fd;
+
+	char                *map;
+	int                  map_size;
+
 	char                *buf;
 	uint64_t             sec;
 
 	int                  error;
-	int                  request_pending;
+	unsigned long        secs_pending;
 	unsigned long        blk_size;
 
 	struct vhd_info      info;
@@ -38,6 +43,8 @@ struct vhd_context {
 static void
 free_vhd_context(struct vhd_context *ctx)
 {
+	close(ctx->fd);
+	free(ctx->map);
 	free(ctx->buf);
 	free(ctx->info.bat);
 	tapdisk_free_queue(&ctx->dd->td_state->queue);
@@ -54,6 +61,19 @@ init_vhd_context(struct vhd_context *ctx, struct disk_driver *dd)
 	ctx->error    = vhd_get_bat(dd, &(ctx->info));
 	if (ctx->error)
 		return ctx->error;
+
+	ctx->fd = open(dd->name, O_RDONLY | O_DIRECT | O_LARGEFILE);
+	if (ctx->fd == -1) {
+		free(ctx->info.bat);
+		return errno;
+	}
+
+	ctx->map_size = ((ctx->info.spb >> 3) + 511) & ~511;
+	if (posix_memalign((void **)&ctx->map, 512, ctx->map_size)) {
+		ctx->map   = NULL;
+		ctx->error = -ENOMEM;
+		goto fail;
+	}
 
 	ctx->error    = tapdisk_init_queue(queue,
 					   TAPDISK_DATA_REQUESTS + 
@@ -89,7 +109,7 @@ wait_for_responses(struct vhd_context *ctx)
 	struct disk_driver *dd = ctx->dd;
 	struct tqueue *queue   = &dd->td_state->queue;
 
-	if (!ctx->request_pending || ctx->error)
+	if (!ctx->secs_pending || ctx->error)
 		return;
 
 	FD_ZERO(&readfds);
@@ -102,13 +122,87 @@ wait_for_responses(struct vhd_context *ctx)
 }
 
 static int
+read_bitmap(struct vhd_context *ctx, unsigned long long blk)
+{
+	off64_t off = ctx->info.bat[blk] << SECTOR_SHIFT;
+
+	if (lseek64(ctx->fd, off, SEEK_SET) == (off64_t)-1)
+		return -errno;
+
+	if (read(ctx->fd, ctx->map, ctx->map_size) != ctx->map_size)
+		return -errno;
+
+	return 0;
+}
+
+static int
+bitmap_full(struct vhd_context *ctx)
+{
+	int i;
+
+	for (i = 0; i < ctx->map_size; i++)
+		if (ctx->map[i] != (char)0xFF)
+			return 0;
+
+	return 1;
+}
+
+static int
+finish_read(struct disk_driver *dd, int res, 
+	     uint64_t sec, int nr_secs, int idx, void *private)
+{
+	int err = (res == BLK_NOT_ALLOCATED ? 0 : res);
+	struct vhd_context *ctx = (struct vhd_context *)private;
+
+	ctx->error = (ctx->error ? : err);
+	ctx->secs_pending -= nr_secs;
+
+	return err;
+}
+
+static int
+read_block(struct vhd_context *ctx, unsigned long long blk)
+{
+	int ret;
+	unsigned long secs;
+	unsigned long long sec;
+	struct disk_driver *dd;
+	struct tqueue *queue;
+
+	memset(ctx->buf, 0, ctx->blk_size);
+
+ again:
+	dd    = ctx->dd;
+	sec   = blk_to_sec(ctx, blk);
+	secs  = ctx->blk_size >> SECTOR_SHIFT;
+	queue = &dd->td_state->queue;
+	ctx->secs_pending = secs;
+	ctx->error = 0;
+
+	ctx->error = dd->drv->td_queue_read(dd, sec, secs, ctx->buf,
+					    finish_read, 0, (void *)ctx);
+
+	if (!ctx->error) {
+		do {
+			tapdisk_submit_all_tiocbs(queue);
+			wait_for_responses(ctx);
+		} while (ctx->secs_pending);
+	}
+
+	if (ctx->error == -EBUSY)
+		goto again;
+
+	return ctx->error;
+}
+
+static int
 finish_write(struct disk_driver *dd, int res, 
 	     uint64_t sec, int nr_secs, int idx, void *private)
 {
 	struct vhd_context *ctx = (struct vhd_context *)private;
 
-	ctx->error = res;
-	ctx->request_pending = 0;
+	ctx->error = (ctx->error ? : res);
+	ctx->secs_pending -= nr_secs;
 
 	return res;
 }
@@ -122,20 +216,27 @@ fill_block(struct vhd_context *ctx, unsigned long long blk)
 	struct disk_driver *dd;
 	struct tqueue *queue;
 
-	/*
-	 * presently, vhd preallocates entire 2MB blocks,
-	 * so if the block is allocated, we don't need to fill it.
-	 * NB: this will need to change if vhd ceases to preallocate.
-	 */
-	if (ctx->info.bat[blk] != DD_BLK_UNUSED)
-		return 0;
-	
+	if (ctx->info.bat[blk] != DD_BLK_UNUSED) {
+		ctx->error = read_bitmap(ctx, blk);
+		if (ctx->error)
+			return ctx->error;
+
+		if (bitmap_full(ctx))
+			/* no need to fill this block */
+			return 0;
+		else {
+			ctx->error = read_block(ctx, blk);
+			if (ctx->error)
+				return ctx->error;
+		}
+	}
+
  again:
 	dd    = ctx->dd;
 	sec   = blk_to_sec(ctx, blk);
 	secs  = ctx->blk_size >> SECTOR_SHIFT;
 	queue = &dd->td_state->queue;
-	ctx->request_pending = 1;
+	ctx->secs_pending = secs;
 	ctx->error = 0;
 
 	ctx->error = dd->drv->td_queue_write(dd, sec, secs, ctx->buf, 
@@ -145,7 +246,7 @@ fill_block(struct vhd_context *ctx, unsigned long long blk)
 		do {
 			tapdisk_submit_all_tiocbs(queue);
 			wait_for_responses(ctx);
-		} while (ctx->request_pending);
+		} while (ctx->secs_pending);
 	}
 
 	if (ctx->error == -EBUSY)
@@ -200,7 +301,7 @@ vhd_fill(char *path)
 	for (i = 0; i < ctx.info.bat_entries && !ctx.error; i++)
 		fill_block(&ctx, i);
 
-	while (ctx.request_pending && !ctx.error)
+	while (ctx.secs_pending && !ctx.error)
 		wait_for_responses(&ctx);
 
  done:
