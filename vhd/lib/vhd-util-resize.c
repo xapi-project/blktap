315666c48a8721e0c10919cf676de5ada6c12b72
Jake Wires <jake.wires@citrix.com>
2008-07-16 22:09:11 -0700

    disable vhd-resize pending further testing

--- vhd/lib/vhd-util-resize.c
+++ vhd/lib/vhd-util-resize.c
@@ -1005,6 +1005,9 @@ vhd_util_resize(int argc, char **argv)
 	vhd_journal_t journal;
 	vhd_context_t *vhd;
 
+	printf("resize not yet implemented\n");
+	return -ENOSYS;
+
 	err  = -EINVAL;
 	size = 0;
 	name = NULL;
