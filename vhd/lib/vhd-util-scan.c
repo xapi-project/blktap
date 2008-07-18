1cf93223cfc0d041b62255268191d1abe868e0fe
Jake Wires <jake.wires@citrix.com>
2008-07-18 16:47:37 -0700

    implement -p option for vhd-util scan to display vhd genealogy hierarchies

--- vhd/lib/vhd-util-scan.c
+++ vhd/lib/vhd-util-scan.c
@@ -12,12 +12,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "list.h"
 #include "libvhd.h"
 
-#define VHD_SCAN_FAST       0x01
-#define VHD_SCAN_PRETTY     0x02
-#define VHD_SCAN_VOLUMES    0x04
-#define VHD_SCAN_NOFAIL     0x08
+#define VHD_SCAN_FAST        0x01
+#define VHD_SCAN_PRETTY      0x02
+#define VHD_SCAN_VOLUMES     0x04
+#define VHD_SCAN_NOFAIL      0x08
+#define VHD_SCAN_VERBOSE     0x10
 
 struct vhd_image {
 	char                *name;
@@ -27,20 +29,244 @@ struct vhd_image {
 	uint8_t              hidden;
 	int                  error;
 	char                *message;
+
+	struct list_head     sibling;
+	struct list_head     children;
+	struct vhd_image    *parent_image;
+};
+
+struct vhd_scan {
+	int                  cur;
+	int                  size;
+	struct vhd_image   **images;
 };
 
 static int flags;
+static struct vhd_scan scan;
+
+static int
+vhd_util_scan_pretty_allocate_list(int cnt)
+{
+	int i;
+
+	scan.images = calloc(cnt, sizeof(struct vhd_image *));
+	if (!scan.images)
+		return -ENOMEM;
+
+	/* 
+	 * allocate each image individually
+	 * so qsort won't mess up linked lists
+	 */
+	for (i = 0; i < cnt; i++) {
+		scan.images[i] = calloc(1, sizeof(struct vhd_image));
+		if (!scan.images[i])
+			goto fail;
+	}
+
+	scan.cur  = 0;
+	scan.size = cnt;
+
+	return 0;
+
+fail:
+	for (i = 0; i < cnt; i++)
+		free(scan.images[i]);
+	free(scan.images);
+	return -ENOMEM;
+}
+
+static void
+vhd_util_scan_pretty_free_list(void)
+{
+	int i;
+
+	if (!scan.size)
+		return;
+
+	for (i = 0; i < scan.size; i++)
+		free(scan.images[i]);
+
+	free(scan.images);
+	memset(&scan, 0, sizeof(scan));
+}
+
+static int
+vhd_util_scan_pretty_add_image(struct vhd_image *image)
+{
+	int i;
+	struct vhd_image *img;
+
+	for (i = 0; i < scan.cur; i++) {
+		img = scan.images[i];
+		if (!strcmp(img->name, image->name))
+			return 0;
+	}
+
+	if (scan.cur >= scan.size)
+		return -ENOSPC;
+
+	img = scan.images[scan.cur];
+	INIT_LIST_HEAD(&img->sibling);
+	INIT_LIST_HEAD(&img->children);
+
+	img->capacity = image->capacity;
+	img->size     = image->size;
+	img->hidden   = image->hidden;
+	img->error    = image->error;
+	img->message  = image->message;
+
+	img->name = strdup(image->name);
+	if (!img->name)
+		goto fail;
+
+	if (image->parent) {
+		img->parent = strdup(image->parent);
+		if (!img->parent)
+			goto fail;
+	}
+
+	scan.cur++;
+	return 0;
+
+fail:
+	free(img->name);
+	free(img->parent);
+	memset(img, 0, sizeof(*img));
+	return -ENOMEM;
+}
+
+static int
+vhd_util_scan_pretty_image_compare(const void *lhs, const void *rhs)
+{
+	struct vhd_image *l, *r;
+
+	l = *(struct vhd_image **)lhs;
+	r = *(struct vhd_image **)rhs;
+
+	return strcmp(l->name, r->name);
+}
+
+static void
+vhd_util_scan_print_image_indent(struct vhd_image *image, int tab)
+{
+	char *pad, *name, *pmsg, *parent;
+
+	pad    = (tab ? " " : "");
+	name   = image->name;
+	parent = (image->parent ? : "none");
+
+	if ((flags & VHD_SCAN_PRETTY) && image->parent && !image->parent_image)
+		pmsg = " (not found in scan)";
+	else
+		pmsg = "";
+
+	if (!(flags & VHD_SCAN_VERBOSE)) {
+		name = basename(image->name);
+		if (image->parent)
+			parent = basename(image->parent);
+	}
+
+	if (image->error)
+		printf("%*svhd=%s scan-error=%d error-message='%s'\n",
+		       tab, pad, image->name, image->error, image->message);
+	else
+		printf("%*svhd=%s capacity=%llu size=%llu hidden=%u "
+		       "parent=%s%s\n", tab, pad, name, image->capacity,
+		       image->size, image->hidden, parent, pmsg);
+}
+
+static void
+vhd_util_scan_pretty_print_tree(struct vhd_image *image, int depth)
+{
+	struct vhd_image *img, *tmp;
+
+	vhd_util_scan_print_image_indent(image, depth * 3);
+
+	list_for_each_entry_safe(img, tmp, &image->children, sibling)
+		if (!img->hidden)
+			vhd_util_scan_pretty_print_tree(img, depth + 1);
+
+	list_for_each_entry_safe(img, tmp, &image->children, sibling)
+		if (img->hidden)
+			vhd_util_scan_pretty_print_tree(img, depth + 1);
+
+	free(image->name);
+	free(image->parent);
+
+	image->name   = NULL;
+	image->parent = NULL;
+}
+
+static void
+vhd_util_scan_pretty_print_images(void)
+{
+	int i;
+	struct vhd_image *image, **parentp, *parent, *keyp, key;
+
+	qsort(scan.images, scan.cur, sizeof(scan.images[0]),
+	      vhd_util_scan_pretty_image_compare);
+
+	for (i = 0; i < scan.cur; i++) {
+		image = scan.images[i];
+
+		if (!image->parent) {
+			image->parent_image = NULL;
+			continue;
+		}
+
+		memset(&key, 0, sizeof(key));
+		key.name = image->parent;
+		keyp     = &key;
+
+		parentp  = bsearch(&keyp, scan.images, scan.cur,
+				   sizeof(scan.images[0]),
+				   vhd_util_scan_pretty_image_compare);
+		if (!parentp) {
+			image->parent_image = NULL;
+			continue;
+		}
+
+		parent = *parentp;
+		image->parent_image = parent;
+		list_add_tail(&image->sibling, &parent->children);
+	}
+
+	for (i = 0; i < scan.cur; i++) {
+		image = scan.images[i];
+
+		if (image->parent_image)
+			continue;
+
+		vhd_util_scan_pretty_print_tree(image, 0);
+	}
+
+	for (i = 0; i < scan.cur; i++) {
+		image = scan.images[i];
+
+		if (!image->name)
+			continue;
+
+		vhd_util_scan_pretty_print_tree(image, 0);
+	}
+}
 
 static void
 vhd_util_scan_print_image(struct vhd_image *image)
 {
-	if (image->error)
-		printf("vhd=%s scan-error=%d error-message='%s'\n",
-		       image->name, image->error, image->message);
-	else
-		printf("vhd=%s capacity=%llu size=%llu hidden=%u parent=%s\n",
-		       image->name, image->capacity, image->size,
-		       image->hidden, (image->parent ? : "none"));
+	int err;
+
+	if (flags & VHD_SCAN_PRETTY) {
+		err = vhd_util_scan_pretty_add_image(image);
+		if (!err)
+			return;
+
+		if (!image->error) {
+			image->error   = err;
+			image->message = "allocating memory";
+		}
+	}
+
+	vhd_util_scan_print_image_indent(image, 0);
 }
 
 static int
@@ -123,7 +349,18 @@ vhd_util_scan_files(int cnt, char **files)
 		memset(&vhd, 0, sizeof(vhd));
 		memset(&image, 0, sizeof(image));
 
-		image.name = files[i];
+		if (!(flags & VHD_SCAN_PRETTY))
+			image.name = files[i];
+		else {
+			image.name = realpath(files[i], NULL);
+			if (!image.name) {
+				ret           = -EAGAIN;
+				image.name    = files[i];
+				image.message = "resolving name";
+				image.error   = -errno;
+				goto end;
+			}
+		}
 
 		err = vhd_open(&vhd, image.name, vhd_flags);
 		if (err) {
@@ -175,11 +412,16 @@ vhd_util_scan_files(int cnt, char **files)
 
 		if (vhd.file)
 			vhd_close(&vhd);
+		if (image.name != files[i])
+			free(image.name);
 		free(image.parent);
 
 		if (err && !(flags & VHD_SCAN_NOFAIL))
 			break;
 	}
+
+	if (flags & VHD_SCAN_PRETTY)
+		vhd_util_scan_pretty_print_images();
 
 	if (flags & VHD_SCAN_NOFAIL)
 		return ret;
@@ -200,7 +442,7 @@ vhd_util_scan(int argc, char **argv)
 	filter = NULL;
 
 	optind = 0;
-	while ((c = getopt(argc, argv, "m:fclph")) != -1) {
+	while ((c = getopt(argc, argv, "m:fclpvh")) != -1) {
 		switch (c) {
 		case 'm':
 			filter = optarg;
@@ -215,8 +457,10 @@ vhd_util_scan(int argc, char **argv)
 			flags |= VHD_SCAN_VOLUMES;
 			break;
 		case 'p':
-			printf("pretty scan not yet implemented\n");
 			flags |= VHD_SCAN_PRETTY;
+			break;
+		case 'v':
+			flags |= VHD_SCAN_VERBOSE;
 			break;
 		case 'h':
 			goto usage;
@@ -229,6 +473,9 @@ vhd_util_scan(int argc, char **argv)
 	cnt   = 0;
 	files = NULL;
 	memset(&g, 0, sizeof(g));
+
+	if (flags & VHD_SCAN_PRETTY)
+		flags &= ~VHD_SCAN_FAST;
 
 	if (filter) {
 		int gflags = ((flags & VHD_SCAN_FAST) ? GLOB_NOSORT : 0);
@@ -253,11 +500,21 @@ vhd_util_scan(int argc, char **argv)
 	memcpy(files + g.gl_pathc,
 	       argv + optind, (argc - optind) * sizeof(char *));
 
+	if (flags & VHD_SCAN_PRETTY) {
+		err = vhd_util_scan_pretty_allocate_list(cnt);
+		if (err) {
+			printf("scan failed: no memory\n");
+			return ((flags & VHD_SCAN_NOFAIL) ? EAGAIN : ENOMEM);
+		}
+	}
+
 	err = vhd_util_scan_files(cnt, files);
 	if (err)
 		ret = -EAGAIN;
 
 	free(files);
+	if (flags & VHD_SCAN_PRETTY)
+		vhd_util_scan_pretty_free_list();
 
 	if (flags & VHD_SCAN_NOFAIL)
 		return ret;
@@ -267,6 +524,6 @@ vhd_util_scan(int argc, char **argv)
 usage:
 	printf("usage: [OPTIONS] FILES\n"
 	       "options: [-m match filter] [-f fast] [-c continue on failure] "
-	       "[-l LVM volumes] [-p pretty print] [-h help]\n");
+	       "[-l LVM volumes] [-p pretty print] [-v verbose] [-h help]\n");
 	return err;
 }
