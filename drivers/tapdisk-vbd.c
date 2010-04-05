8f6ad1c60a861428c1f3f9c893aa8e441c554436
Daniel Stodden <daniel.stodden@citrix.com>
2010-04-05 12:35:16 -0700

    CA-29373: Stop retrying the pathetic case.
    Separating the retryable from the recoverable errors should make the
    control path more responsive to broken SRs or images. Fail vreqs with
    irrecoverable errors immediately. Presently known ones comprise ESTALE
    and ENOSPC. Both don't have a great prospect to improve within the
    next two minutes.
    
    Note that this somewhat obsoletes ENOSPC, as our original reason for
    adding a tapdisk-level forced-shutdown. However, there may still be
    reasons to discard retryable requests.

--- drivers/tapdisk-vbd.c
+++ drivers/tapdisk-vbd.c
@@ -1309,17 +1309,40 @@ tapdisk_vbd_check_queue(td_vbd_t *vbd)
 	return 0;
 }
 
+static int
+tapdisk_vbd_request_failed_transient(td_vbd_t *vbd, td_vbd_request_t *vreq)
+{
+	if (vreq->status != BLKIF_RSP_ERROR)
+		return 0;
+
+	if (td_flag_test(vbd->state, TD_VBD_DEAD) ||
+	    td_flag_test(vbd->state, TD_VBD_SHUTDOWN_REQUESTED))
+		return 0;
+
+	switch (abs(vreq->error)) {
+	case ESTALE:
+	case ENOSPC:
+		return 0;
+	}
+
+	if (tapdisk_vbd_request_timeout(vreq))
+		return 0;
+
+	return 1;
+}
+
 static void
 tapdisk_vbd_complete_vbd_request(td_vbd_t *vbd, td_vbd_request_t *vreq)
 {
 	if (!vreq->submitting && !vreq->secs_pending) {
-		if (vreq->status == BLKIF_RSP_ERROR &&
-		    !tapdisk_vbd_request_timeout(vreq) &&
-		    !td_flag_test(vbd->state, TD_VBD_DEAD) &&
-		    !td_flag_test(vbd->state, TD_VBD_SHUTDOWN_REQUESTED))
-			tapdisk_vbd_move_request(vreq, &vbd->failed_requests);
+		struct list_head *dest;
+
+		if (tapdisk_vbd_request_failed_transient(vbd, vreq))
+			dest = &vbd->failed_requests;
 		else
-			tapdisk_vbd_move_request(vreq, &vbd->completed_requests);
+			dest = &vbd->completed_requests;
+
+		tapdisk_vbd_move_request(vreq, dest);
 	}
 }
 
