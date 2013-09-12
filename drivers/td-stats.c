267aa785b04a41beef87eb156f5b63989071e68e
Thanos Makatos <thanos.makatos@citrix.com>
2013-09-12 16:45:48 +0100

    EA-1158/CP-5965: Don't forward READs when in mirroring mode.
    Also, fix a tap-ctl-stats bug, plus minor code clean up.
    
    Signed-off-by: Thanos Makatos <thanos.makatos@citrix.com>

--- drivers/td-stats.c
+++ drivers/td-stats.c
@@ -30,7 +30,7 @@ __tapdisk_xenblkif_stats(struct td_xenblkif * blkif, td_stats_t * st)
     assert(st);
     assert(blkif->ctx);
 
-    tapdisk_stats_field(st, "pool", blkif->ctx->pool);
+    tapdisk_stats_field(st, "pool", "s", blkif->ctx->pool);
     tapdisk_stats_field(st, "domid", "d", blkif->domid);
     tapdisk_stats_field(st, "devid", "d", blkif->devid);
 
@@ -47,9 +47,9 @@ __tapdisk_xenblkif_stats(struct td_xenblkif * blkif, td_stats_t * st)
     tapdisk_stats_field(st, "errors", "{");
     tapdisk_stats_field(st, "msg", "llu", blkif->stats.errors.msg);
     tapdisk_stats_field(st, "map", "llu", blkif->stats.errors.map);
-    tapdisk_stats_field(st, "vbq", "llu", blkif->stats.errors.vbd);
+    tapdisk_stats_field(st, "vbd", "llu", blkif->stats.errors.vbd);
     tapdisk_stats_field(st, "img", "llu", blkif->stats.errors.img);
-    tapdisk_stats_leave(st, ']');
+    tapdisk_stats_leave(st, '}');
 }
 
 void
