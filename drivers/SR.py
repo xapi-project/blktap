a29015e5778faf9424ef7fa756992e2e3d11dddd
Zheng Li <zheng.li@eu.citrix.com>
2013-09-18 12:58:49 +0000

    CP-6167: Fix a bug reported by pylint
    There is no variable 'uuid' in scope.
    
    Signed-off-by: Zheng Li <zheng.li@eu.citrix.com>

--- drivers/SR.py
+++ drivers/SR.py
@@ -559,7 +559,7 @@ class ScanRecord:
                 self.sr.forget_vdi(vdi['uuid'])
             except XenAPI.Failure, e:
                 if e.details == "HANDLE_INVALID" or e.details == "UUID_INVALID":
-                   util.SMlog("VDI %s not found, ignoring exception" % uuid)
+                   util.SMlog("VDI %s not found, ignoring exception" % vdi['uuid'])
                 else:
                    raise
 
