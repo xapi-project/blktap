71260e7cd9f623eb01d0bcaa5ac4030aa3ef7ee4
Thanos Makatos <thanos.makatos@citrix.com>
2014-04-08 13:45:40 +0100

    CA-130606: move PAGE_XXX symbols into libtapdisk
    Symbols PAGE_MASK, PAGE_SIZE, and PAGE_SHIFT are defined in tapdisk2.c
    which is not compiled into libtapdisk, hence these symbols do not get
    defined in the library and that obviously doesn't link.
    
    Signed-off-by: Thanos Makatos <thanos.makatos@citrix.com>

--- drivers/tapdisk2.c
+++ drivers/tapdisk2.c
@@ -30,10 +30,6 @@
 #include "tapdisk-server.h"
 #include "tapdisk-control.h"
 
-unsigned int PAGE_SIZE;
-unsigned int PAGE_MASK;
-unsigned int PAGE_SHIFT;
-
 void tdnbd_fdreceiver_start();
 void tdnbd_fdreceiver_stop();
 
@@ -75,12 +71,6 @@ main(int argc, char *argv[])
 	char *control;
 	int c, err, nodaemon;
 	FILE *out;
-    unsigned int i = 0;
-
-    PAGE_SIZE = sysconf(_SC_PAGESIZE);
-    PAGE_MASK = ~(PAGE_SIZE - 1);
-
-    for (i = PAGE_SIZE, PAGE_SHIFT = 0; i > 1; i >>= 1, PAGE_SHIFT++);
 
 	control  = NULL;
 	nodaemon = 0;
