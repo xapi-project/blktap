6257865d64733694144bcb6ad0d82f20a4e29f54
Mate Lakat <mate.lakat@citrix.com>
2014-07-24 09:25:51 +0100

    CA-140596: [UnitTest] Test file not found in Lock
    These are test cases covering the lock module, and also testing what
    happens if file() raises an IOError. This can happen as a race
    condition, if two locks - A and B are created in the same namespace and
    by the time A gets to open the lockfile, B already cleaned up the
    namespace.
    
    Signed-off-by: Mate Lakat <mate.lakat@citrix.com>
    Acked-by: Germano Percossi <germano.percossi@citrix.com>
    Imported-by: Thanos Makatos <thanos.makatos@citrix.com>
    
    (cherry picked from commit d10e3b034486a852483c22822e16b6249aeefdca)

--- tests/test_lock.py
+++ tests/test_lock.py
@@ -2,6 +2,7 @@ import unittest
 import mock
 import os
 import gc
+import errno
 
 import testlib
 
@@ -11,6 +12,65 @@ import lock
 class FailingOpenContext(testlib.TestContext):
     def fake_open(self, fname, mode='r'):
         raise IOError()
+
+
+class TestLock(unittest.TestCase):
+    @testlib.with_context
+    def test_lock_without_namespace_creates_nil_namespace(self, context):
+        lck = lock.Lock('somename')
+
+        self.assertTrue(
+            os.path.exists(
+                os.path.join(lck.BASE_DIR, '.nil')))
+
+    @testlib.with_context
+    def test_lock_with_namespace_creates_namespace(self, context):
+        lck = lock.Lock('somename', ns='namespace')
+
+        self.assertTrue(
+            os.path.exists(
+                os.path.join(lck.BASE_DIR, 'namespace')))
+
+    @testlib.with_context
+    def test_lock_without_namespace_creates_file(self, context):
+        lck = lock.Lock('somename')
+
+        self.assertTrue(
+            os.path.exists(
+                os.path.join(lck.BASE_DIR, '.nil', 'somename')))
+
+    @testlib.with_context
+    def test_lock_with_namespace_creates_file(self, context):
+        lck = lock.Lock('somename', ns='namespace')
+
+        self.assertTrue(
+            os.path.exists(
+                os.path.join(lck.BASE_DIR, 'namespace', 'somename')))
+
+    @testlib.with_context
+    def test_lock_file_create_fails_retried(self, context):
+        Lock = create_lock_class_that_fails_to_create_file(1)
+        lck = Lock('somename', ns='namespace')
+
+        self.assertTrue(
+            os.path.exists(
+                os.path.join(lck.BASE_DIR, 'namespace', 'somename')))
+
+
+def create_lock_class_that_fails_to_create_file(number_of_failures):
+
+    class LockThatFailsToCreateFile(lock.Lock):
+        _failures = number_of_failures
+
+        def _open_lockfile(self):
+            if self._failures > 0:
+                error = IOError('No such file')
+                error.errno = errno.ENOENT
+                self._failures -= 1
+                raise error
+            return lock.Lock._open_lockfile(self)
+
+    return LockThatFailsToCreateFile
 
 
 class TestLockDestruction(unittest.TestCase):
