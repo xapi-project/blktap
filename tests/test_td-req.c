f7996085a75ed45b47e8bb90ae1d5fcf0697bfdf
Mate Lakat <mate.lakat@citrix.com>
2014-09-11 11:10:36 +0100

    CA-147141: Add example noop unit tests
    Signed-off-by: Mate Lakat <mate.lakat@citrix.com>

---
+++ tests/test_td-req.c
@@ -0,0 +1,30 @@
+#include "unity.h"
+#include "drivers/tapdisk.h"
+#include "mock_tapdisk-stats.h"
+#include "mock_tapdisk-interface.h"
+#include <xenctrl.h>
+#include "drivers/td-req.h"
+#include "drivers/tapdisk-utils.h"
+#include "mock_td-ctx.h"
+#include "mock_td-blkif.h"
+#include "mock_tapdisk-server.h"
+#include "mock_tapdisk-driver.h"
+#include "mock_tapdisk-log.h"
+#include "mock_tapdisk-vbd.h"
+#include "mock_tapdisk-image.h"
+
+unsigned int PAGE_SHIFT;
+
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void testDummy(void)
+{
+}
+
