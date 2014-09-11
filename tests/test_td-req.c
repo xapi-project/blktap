0dc13ee12a48e0859c59bf257110fa9d69c5d48b
Mate Lakat <mate.lakat@citrix.com>
2014-09-11 15:00:07 +0100

    CA-147141: Test tapdisk_xenblkif_complete_request
    Signed-off-by: Mate Lakat <mate.lakat@citrix.com>

--- tests/test_td-req.c
+++ tests/test_td-req.c
@@ -18,13 +18,56 @@ unsigned int PAGE_SHIFT;
 
 void setUp(void)
 {
+    tapdisk_server_mem_mode_IgnoreAndReturn(NORMAL_MEMORY_MODE);
 }
 
 void tearDown(void)
 {
 }
 
-void testDummy(void)
-{
+#define RING_SIZE 20
+
+struct td_xenblkif* create_dead_blkif(void) {
+    struct td_xenblkif* blkif;
+    blkif_request_t* free_requests;
+
+    blkif = malloc(sizeof(struct td_xenblkif));
+    free_requests = malloc(RING_SIZE * sizeof(blkif_request_t));
+
+    blkif->reqs_bufcache = malloc(RING_SIZE * sizeof(void*));
+    blkif->n_reqs_bufcache_free = 0;
+
+    blkif->dead = 1;
+    blkif->n_reqs_free = 10;
+    blkif->ring_size = RING_SIZE;
+    blkif->reqs_free = free_requests;
+
+    return blkif;
 }
 
+
+void testCompleteRequestGenericRequestOndeadRingDoesNotDestroyRing(void)
+{
+    struct td_xenblkif_req request;
+    struct td_xenblkif* blkif;
+
+    blkif = create_dead_blkif();
+
+    tapdisk_xenblkif_reqs_pending_IgnoreAndReturn(1);
+
+    tapdisk_xenblkif_complete_request(blkif, &request, 0, 0);
+}
+
+
+void testCompleteRequestLastRequestOnDeadringDestroysRing(void)
+{
+    struct td_xenblkif_req request;
+    struct td_xenblkif* blkif;
+
+    blkif = create_dead_blkif();
+
+    tapdisk_xenblkif_reqs_pending_IgnoreAndReturn(0);
+
+    tapdisk_xenblkif_destroy_ExpectAndReturn(blkif, 0);
+    tapdisk_xenblkif_complete_request(blkif, &request, 0, 0);
+}
