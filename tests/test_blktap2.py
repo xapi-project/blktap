955e4a743db9bc26f15eb3f8f9bb78e74436ad3d
Flavien Quesnel <flavien.quesnel@citrix.com>
2014-10-03 14:57:23 +0100

    CA-147816: [UnitTest] Update and refactor unit tests
    Update and refactor unit tests introduced by 3060073a to reflect the
    changes from a121c306 to c0f26bf4.
    
    Signed-off-by: Flavien Quesnel <flavien.quesnel@citrix.com>
    Imported-by: Thanos Makatos <thanos.makatos@citrix.com>
    
    (cherry picked from commit c71c96858a4e725166808e05d44c5c386ac813da)

--- tests/test_blktap2.py
+++ tests/test_blktap2.py
@@ -7,31 +7,21 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.VDI.TargetDriver')
     def setUp(self, mock_target):
         mock_target.get_vdi_type.return_value = 'phy'
-        mock_target.vdi.sr.sm_config = {'type': 'cd'}
+
+        def mock_handles(type_str):
+            return type_str == 'udev'
+
+        mock_target.vdi.sr.handles = mock_handles
 
         self.vdi = blktap2.VDI('uuid', mock_target, None)
         self.vdi.target = mock_target
 
-    def test_tap_wanted_returns_true_for_physical_dvd_drive(self):
+    def test_tap_wanted_returns_true_for_udev_device(self):
         result = self.vdi.tap_wanted()
 
         self.assertEquals(True, result)
 
-    @mock.patch('blktap2.VDI.get_sr_sm_config')
-    def test_tap_wanted_returns_true_for_dvd_drive_if_sm_config_deleted(
-            self,
-            mock_sr_sm_config):
-        mock_sr_sm_config.return_value = {'type': 'cd'}
-        del self.vdi.target.vdi.sr.sm_config
-
-        sm_config_exists = hasattr(self.vdi.target.vdi.sr, 'sm_config')
-        self.assertEquals(False, sm_config_exists)
-
-        result = self.vdi.tap_wanted()
-
-        self.assertEquals(True, result)
-
-    def test_get_tap_type_returns_aio_for_physical_dvd_drive(self):
+    def test_get_tap_type_returns_aio_for_udev_device(self):
         result = self.vdi.get_tap_type()
 
         self.assertEquals('aio', result)
