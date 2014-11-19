b5c7890a60b968bae4450b7f1b20cc4118e4cd32
Thanos Makatos <thanos.makatos@citrix.com>
2014-11-19 11:26:41 +0000

    CA-150952: properly check for XAPI session existence
    Don't check if a XAPI session exists by comparing session with the empty
    string because if a XAPI session does exist it returns a error string,
    which evaluates to true.
    
    Signed-off-by: Thanos Makatos <thanos.makatos@citrix.com>
    
    (cherry picked from commit 3fd6375cc13cb4106153b15c31c62580fd7dcc89)

--- drivers/util.py
+++ drivers/util.py
@@ -1696,7 +1696,7 @@ def get_pool_restrictions(session):
 
 def read_caching_is_restricted(session):
     """Tells whether read caching is restricted."""
-    if session is None or session == "":
+    if session is None or (isinstance(session, str) and session == ""):
         return True
     restrictions = get_pool_restrictions(session)
     if 'restrict_read_caching' in restrictions and \
