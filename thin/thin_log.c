a85bbc915c27f7cc00a38cf9fc091fd9a4ef9677
Stefano Panella <Stefano.Panella@citrix.com>
2015-09-24 11:44:57 +0100

    CP-12850: Refactor thinprovd_log code to be less specific
    This so we can use previous thinprovd_log stuff in a more
    general way and not only for thinprovd.
    
    - thinprovd_log[c,h] renamed to thin_log[c,h]
    - all THINPROVD_LOG renamed to THIN_LOG
    - all thinpd_ renamed to thin_
    - thin_openlog now accepts a string for the log name
      so it can be used for different modules/executables
    
    Signed-off-by: Stefano Panella <stefano.panella@citrix.com>
    Reviewed-by: Germano Percossi <germano.percossi@citrix.com>

--- thin/thin_log.c
+++ thin/thin_log.c
@@ -1,26 +1,24 @@
 /* This define is needed for vsyslog(). */
 #define _BSD_SOURCE
 
-#include "thinprovd_log.h"
+#include "thin_log.h"
 
 #include <syslog.h>
 #include <stdarg.h>
 
-#define THINPROVD "THINPROVD"
-
 static int
-thinpd_match_log_level(enum THINPD_LOG_LEVEL tpd_log_level)
+thin_match_log_level(enum THIN_LOG_LEVEL tpd_log_level)
 {
     int log_level;
 
     switch (tpd_log_level) {
-    case THINPD_LOG_ERR:
+    case THIN_LOG_ERR:
         log_level = LOG_EMERG;
         break;
-    case THINPD_LOG_INFO:
+    case THIN_LOG_INFO:
         log_level = LOG_INFO;
         break;
-    case THINPD_LOG_DBG:
+    case THIN_LOG_DBG:
         log_level = LOG_DEBUG;
         break;
     default:
@@ -31,58 +29,58 @@ thinpd_match_log_level(enum THINPD_LOG_LEVEL tpd_log_level)
 }
 
 inline void
-thinpd_openlog(void)
+thin_openlog(char *logname)
 {
-    openlog(THINPROVD, LOG_CONS | LOG_PID, LOG_DAEMON);
+    openlog(logname, LOG_CONS | LOG_PID, LOG_DAEMON);
 }
 
 inline void
-thinpd_closelog(void)
+thin_closelog(void)
 {
     closelog();
 }
 
 inline void
-thinpd_log_upto(enum THINPD_LOG_LEVEL tpd_log_level)
+thin_log_upto(enum THIN_LOG_LEVEL tpd_log_level)
 {
-    setlogmask(LOG_UPTO(thinpd_match_log_level(tpd_log_level)));
+    setlogmask(LOG_UPTO(thin_match_log_level(tpd_log_level)));
 }
 
 void
-thinpd_log_err(const char *message, ...)
+thin_log_err(const char *message, ...)
 {
     va_list args;
 
     va_start(args, message);
 
-    /* THINPD_LOG_ERR == LOG_EMERG */
+    /* THIN_LOG_ERR == LOG_EMERG */
     vsyslog(LOG_EMERG, message, args);
 
     va_end(args);
 }
 
 void
-thinpd_log_info(const char *message, ...)
+thin_log_info(const char *message, ...)
 
 {
     va_list args;
 
     va_start(args, message);
 
-    /* THINPD_LOG_INFO == LOG_INFO */
+    /* THIN_LOG_INFO == LOG_INFO */
     vsyslog(LOG_INFO, message, args);
 
     va_end(args);
 }
 
 void
-thinpd_log_dbg(const char *message, ...)
+thin_log_dbg(const char *message, ...)
 {
     va_list args;
 
     va_start(args, message);
 
-    /* THINPD_LOG_DBG == LOG_DEBUG */
+    /* THIN_LOG_DBG == LOG_DEBUG */
     vsyslog(LOG_DEBUG, message, args);
 
     va_end(args);
