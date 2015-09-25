xapi-project/blktap

commit 732a6eb742a6d4969cc9fb93b5bc5b466e26192b

CA-172961: Fix plug/unplug for lvmoiscsi/lvmohba
- Code refactored to move functions in derived classes (so they can be
  called in different moments and separately)
- New template for xenvmd conf file

Signed-off-by: Germano Percossi <germano.percossi@citrix.com>

drivers/lvutil.py
@@ -166,7 +166,16 @@ def runxenvmd(uuid,vg,devices):
     global config_dir
     configfile = "%s/%s.xenvmd.config" % (config_dir, vg)
     sockpath = sockpath_of_sr_uuid(uuid)
-    config = "((listenPort None) (listenPath (Some %s)) (host_allocation_quantum 2048) (host_low_water_mark 1024) (vg %s) (devices (%s)) )\n" % (sockpath,vg," ".join(devices))
+    config = """
+(
+ (listenPort ())
+ (listenPath (Some %s))
+ (host_allocation_quantum 2048)
+ (host_low_water_mark 1024)
+ (vg %s)
+ (devices (%s))
+)
+""" % (sockpath,vg," ".join(devices))
     if not os.path.exists(config_dir):
       util.makedirs("/etc/xenvm.d")
     if not os.path.exists(os.path.dirname(sockpath)):
@@ -206,7 +215,6 @@ def runxenvm_local_allocator(uuid, vg, devices, uri):
     util.pread2(cmd) 
     cmd = [ "/bin/xenvm", "host-connect", vg, uuid ]
     util.pread2(cmd)
-    setvginfo(uuid,vg,devices,uri,local_allocator)
 
 def stopxenvm_local_allocator(vg):
     uuid = util.get_this_host ()

drivers/LVHDSR.py
@@ -578,46 +578,6 @@ class LVHDSR(SR.SR):
     def attach(self, uuid):
         util.SMlog("LVHDSR.attach for %s" % self.uuid)
 
-        path_to_file = '/var/run/nonpersistent/sr_alloc_' + uuid
-
-        # If 'allocation' exists in sm_config, overwrite whatever
-        # existed (if anything) in "'sr_alloc_' + uuid".
-        if 'allocation' in self.sm_config:
-            if self.sm_config['allocation'] == 'dynamic':
-                sr_alloc = 'dnmc'
-            elif self.sm_config['allocation'] == 'thick':
-                sr_alloc = 'thck'
-            else:
-                raise Exception("Unknown sm_config['allocation'] value: '{}'."
-                        .format(self.sm_config['allocation']))
-
-            with open(path_to_file, 'w') as f:
-                f.write(sr_alloc)
-
-        # If not, check if the file exists. If it doesn't,
-        # create it and fallback to 'thick'.
-        else:
-            try:
-                f = open(path_to_file, 'r')
-                f.close()
-            except IOError:
-                util.SMlog("File 'sr_alloc_{}' does not exist; Creating it"
-                           " and falling back to 'thick'.".format(uuid))
-                with open(path_to_file, 'w') as f:
-                    f.write('thck')
-
-        vg = self.vgname
-        devices = self.root.split(',')
-
-        sr_alloc = os.getenv('SR_ALLOC')
-        if sr_alloc == "thin":
-            if self.isMaster:
-                lvutil.runxenvmd(uuid, vg, devices)
-
-            uri = "file://local/services/xenvmd/%s" % uuid
-            lvutil.setvginfo(uuid, vg, devices, uri)
-            lvutil.runxenvm_local_allocator(uuid, vg, devices, uri)
-
         self._cleanup(True) # in case of host crashes, if detach wasn't called
 
         if not util.match_uuid(self.uuid) or not lvutil._checkVG(self.vgname):
@@ -1328,6 +1288,64 @@ class LVHDSR(SR.SR):
 
         util.SMlog("Kicking GC")
         cleanup.gc(self.session, self.uuid, True)
+
+
+    def _write_allocation(self, uuid):
+        path_to_file = '/var/run/nonpersistent/sr_alloc_' + uuid
+
+        # If 'allocation' exists in sm_config, overwrite whatever
+        # existed (if anything) in "'sr_alloc_' + uuid".
+        if 'allocation' in self.sm_config:
+            if self.sm_config['allocation'] == 'dynamic':
+                sr_alloc = 'dnmc'
+            elif self.sm_config['allocation'] == 'thick':
+                sr_alloc = 'thck'
+            else:
+                raise Exception("Unknown sm_config['allocation'] value: '{}'."
+                        .format(self.sm_config['allocation']))
+
+            with open(path_to_file, 'w') as f:
+                f.write(sr_alloc)
+
+        # If not, check if the file exists. If it doesn't,
+        # create it and fallback to 'thick'.
+        else:
+            try:
+                f = open(path_to_file, 'r')
+                f.close()
+            except IOError:
+                util.SMlog("File 'sr_alloc_{}' does not exist; Creating it"
+                           " and falling back to 'thick'.".format(uuid))
+                with open(path_to_file, 'w') as f:
+                    f.write('thck')
+
+
+    def _start_xenvmd(self, uuid):
+        sr_alloc = os.getenv('SR_ALLOC')
+        if sr_alloc == "thin":
+            vg = self.vgname
+            devices = self.root.split(',')
+            # move the "if" up!
+            if self.isMaster:
+                lvutil.runxenvmd(uuid, vg, devices)
+
+
+    def _start_local_allocator(self, uuid):
+        sr_alloc = os.getenv('SR_ALLOC')
+        if sr_alloc == "thin":
+            vg = self.vgname
+            devices = self.root.split(',')
+            uri = "file://local/services/xenvmd/%s" % uuid
+            lvutil.runxenvm_local_allocator(uuid, vg, devices, uri)
+
+
+    def _write_vginfo(self, uuid):
+        sr_alloc = os.getenv('SR_ALLOC')
+        if sr_alloc == "thin":
+            vg = self.vgname
+            devices = self.root.split(',')
+            uri = "file://local/services/xenvmd/%s" % uuid
+            lvutil.setvginfo(uuid, vg, devices, uri)
 
 
 class LVHDVDI(VDI.VDI):

drivers/LVHDoHBASR.py
@@ -116,6 +116,10 @@ class LVHDoHBASR(LVHDSR.LVHDSR):
                                              self.sr_ref, self.SCSIid)
 
     def attach(self, sr_uuid):
+        # Write the allocation type on file as soon as we can
+        self._write_allocation(sr_uuid)
+        self._write_vginfo(sr_uuid)
+
         self.hbasr.attach(sr_uuid)
         if self.mpath == "true":
             self.mpathmodule.refresh(self.SCSIid,0)
@@ -123,6 +127,8 @@ class LVHDoHBASR(LVHDSR.LVHDSR):
             path = '/dev/disk/by-scsid/%s' % self.dconf['SCSIid']
             for file in os.listdir(path):
                 self.block_setscheduler('%s/%s' % (path,file))
+
+        self._start_xenvmd(sr_uuid)
 
         self._pathrefresh(LVHDoHBASR)
         if not os.path.exists(self.dconf['device']):
@@ -133,6 +139,7 @@ class LVHDoHBASR(LVHDSR.LVHDSR):
                 self.mpathmodule.refresh(self.SCSIid,0)
         LVHDSR.LVHDSR.attach(self, sr_uuid)
         self._setMultipathableFlag(SCSIid=self.SCSIid)
+        self._start_local_allocator(sr_uuid)
 
     def scan(self, sr_uuid):
         # During a reboot, scan is called ahead of attach, which causes the MGT

drivers/LVHDoISCSISR.py
@@ -472,6 +472,10 @@ class LVHDoISCSISR(LVHDSR.LVHDSR):
             i.detach(sr_uuid)
 
     def attach(self, sr_uuid):
+        # Write the allocation type on file as soon as we can
+        self._write_allocation(sr_uuid)
+        self._write_vginfo(sr_uuid)
+
         try:
             connected = False
             for i in self.iscsiSRs:
@@ -497,6 +501,9 @@ class LVHDoISCSISR(LVHDSR.LVHDSR):
                 # Force a manual bus refresh
                 for a in self.iscsi.adapter:
                     scsiutil.rescan([self.iscsi.adapter[a]])
+
+            self._start_xenvmd(sr_uuid)
+
             self._pathrefresh(LVHDoISCSISR)
             LVHDSR.LVHDSR.attach(self, sr_uuid)
         except Exception, inst:
@@ -504,6 +511,7 @@ class LVHDoISCSISR(LVHDSR.LVHDSR):
                 i.detach(sr_uuid)
             raise xs_errors.XenError("SRUnavailable", opterr=inst)
         self._setMultipathableFlag(SCSIid=self.SCSIid)
+        self._start_local_allocator(sr_uuid)
         
     def detach(self, sr_uuid):
         LVHDSR.LVHDSR.detach(self, sr_uuid)

commit 4517725a2986dbc4092430d443d9d4baf0ff394b

Fix setvginfo call problem introduced in commit 732a6eb7
The two calls to setvginfo got collapsed into one in a
previous commit, but that meant the local allocator
configuration was never set. This commit reinstates the
second call.

It's potentially feasible to collapse the calls again
into one. Something to investigate later.

This patch has had some eyeballing from Germano.

Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>
Acked-by: Germano Percossi <germano.percossi@citrix.com>

drivers/lvutil.py
@@ -317,6 +317,7 @@ def runxenvm_local_allocator(uuid, vg, devices, uri):
     util.pread2(cmd) 
     cmd = [ "/bin/xenvm", "host-connect", vg, uuid ]
     util.pread2(cmd)
+    setvginfo(uuid,vg,devices,uri,local_allocator)
 
 def stopxenvm_local_allocator(vg):
     uuid = util.get_this_host ()