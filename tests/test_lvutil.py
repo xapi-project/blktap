6684e6d55d60bbe87d90564334994937c6880ccc
Pritha Srivastava <pritha.srivastava@citrix.com>
2015-12-09 04:43:13 +0000

    CA-189186: Fixed Unit Tests for lvutil.py
    1)Changed the VG name in the test to the format VG-XenStorage-xxxx,
      because based on this the code infers whether the SR allocation is
      thick or dynamic.
    2)Modified some of the mocked functions.
    
    Signed-off-by: Pritha Srivastava <pritha.srivastava@citrix.com>
    Reviewed-by: Stefano Panella <stefano.panella@citrix.com>
    
    GitHub: closes xapi-project/sm#289

--- tests/test_lvutil.py
+++ tests/test_lvutil.py
@@ -3,6 +3,7 @@ import testlib
 import lvmlib
 import mock
 
+import os
 import lvutil
 
 
@@ -22,9 +23,9 @@ def with_lvm_subsystem(func):
 class TestCreate(unittest.TestCase):
     @with_lvm_subsystem
     def test_create_volume_size(self, lvsystem):
-        lvsystem.add_volume_group('vgroup')
+        lvsystem.add_volume_group('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
-        lvutil.create('volume', 100 * ONE_MEGABYTE, 'vgroup')
+        lvutil.create('volume', 100 * ONE_MEGABYTE, 'VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
         created_lv, = lvsystem.get_logical_volumes_with_name('volume')
 
@@ -32,23 +33,23 @@ class TestCreate(unittest.TestCase):
 
     @with_lvm_subsystem
     def test_create_volume_is_in_the_right_volume_group(self, lvsystem):
-        lvsystem.add_volume_group('vgroup')
+        lvsystem.add_volume_group('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
-        lvutil.create('volume', 100 * ONE_MEGABYTE, 'vgroup')
+        lvutil.create('volume', 100 * ONE_MEGABYTE, 'VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
         created_lv, = lvsystem.get_logical_volumes_with_name('volume')
 
         self.assertEquals(100, created_lv.size_mb)
 
-        self.assertEquals('vgroup', created_lv.volume_group.name)
+        self.assertEquals('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7', created_lv.volume_group.name)
         self.assertTrue(created_lv.active)
         self.assertTrue(created_lv.zeroed)
 
     @with_lvm_subsystem
     def test_create_volume_is_active(self, lvsystem):
-        lvsystem.add_volume_group('vgroup')
+        lvsystem.add_volume_group('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
-        lvutil.create('volume', 100 * ONE_MEGABYTE, 'vgroup')
+        lvutil.create('volume', 100 * ONE_MEGABYTE, 'VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
         created_lv, = lvsystem.get_logical_volumes_with_name('volume')
 
@@ -59,9 +60,9 @@ class TestCreate(unittest.TestCase):
 
     @with_lvm_subsystem
     def test_create_volume_is_zeroed(self, lvsystem):
-        lvsystem.add_volume_group('vgroup')
+        lvsystem.add_volume_group('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
-        lvutil.create('volume', 100 * ONE_MEGABYTE, 'vgroup')
+        lvutil.create('volume', 100 * ONE_MEGABYTE, 'VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
         created_lv, = lvsystem.get_logical_volumes_with_name('volume')
 
@@ -71,37 +72,38 @@ class TestCreate(unittest.TestCase):
 
     @with_lvm_subsystem
     def test_create_creates_logical_volume_with_tags(self, lvsystem):
-        lvsystem.add_volume_group('vgroup')
+        lvsystem.add_volume_group('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
 
-        lvutil.create('volume', ONE_MEGABYTE, 'vgroup', tag='hello')
+        lvutil.create('volume', ONE_MEGABYTE, 'VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7', tag='hello')
 
         created_lv, = lvsystem.get_logical_volumes_with_name('volume')
         self.assertEquals('hello', created_lv.tag)
 
-    @mock.patch('util.pread2')
-    def test_create_percentage_has_precedence_over_size(self, mock_pread2):
-        lvutil.create('volume', ONE_MEGABYTE, 'vgroup',
+    @mock.patch('util.pread')
+    def test_create_percentage_has_precedence_over_size(self, mock_pread):
+        lvutil.create('volume', ONE_MEGABYTE, 'VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7',
                       size_in_percentage="10%F")
 
-        mock_pread2.assert_called_once_with(
-            [lvutil.CMD_LVCREATE] + "-n volume -l 10%F vgroup".split())
-
+        mock_pread.assert_called_once_with(
+            [os.path.join(lvutil.LVM_BIN,lvutil.CMD_LVCREATE)] +
+            "-n volume -l 10%F VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7".split(),
+            quiet=False)
 
 class TestRemove(unittest.TestCase):
     @with_lvm_subsystem
     def test_remove_removes_volume(self, lvsystem):
-        lvsystem.add_volume_group('vgroup')
-        lvsystem.get_volume_group('vgroup').add_volume('volume', 100)
+        lvsystem.add_volume_group('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7')
+        lvsystem.get_volume_group('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7').add_volume('volume', 100)
 
-        lvutil.remove('vgroup/volume')
+        lvutil.remove('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7/volume')
 
         self.assertEquals([], lvsystem.get_logical_volumes_with_name('volume'))
 
     @mock.patch('lvutil._lvmBugCleanup')
-    @mock.patch('util.pread2')
-    def test_remove_additional_config_param(self, mock_pread2, _bugCleanup):
-        lvutil.remove('vgroup/volume', config_param="blah")
-        mock_pread2.assert_called_once_with(
-            [lvutil.CMD_LVREMOVE]
-            + "-f vgroup/volume --config devices{blah}".split()
-        )
+    @mock.patch('util.pread')
+    def test_remove_additional_config_param(self, mock_pread, _bugCleanup):
+        lvutil.remove('VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7/volume', config_param="blah")
+        mock_pread.assert_called_once_with(
+            [os.path.join(lvutil.LVM_BIN, lvutil.CMD_LVREMOVE)]
+            + "-f VG_XenStorage-b3b18d06-b2ba-5b67-f098-3cdd5087a2a7/volume --config devices{blah}".split(),
+           quiet= False)
