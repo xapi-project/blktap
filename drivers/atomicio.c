1372ffe4eeb036577ae4e9084e585ac2e543fcbd
Frediano Ziglio <fziglio@redhat.com>
2017-02-04 22:02:32 +0000

    Do not use K&R C declaration for functions
    K&R syntax is quite obsolete nowadays.
    
    Signed-off-by: Frediano Ziglio <fziglio@redhat.com>

--- drivers/atomicio.c
+++ drivers/atomicio.c
@@ -36,11 +36,10 @@
  * ensure all of data on socket comes through. f==read || f==vwrite
  */
 size_t
-atomicio(f, fd, _s, n)
-	ssize_t (*f) (int, void *, size_t);
-	int fd;
-	void *_s;
-	size_t n;
+atomicio(ssize_t (*f) (int, void *, size_t),
+	 int fd,
+	 void *_s,
+	 size_t n)
 {
 	char *s = _s;
 	size_t pos = 0;
