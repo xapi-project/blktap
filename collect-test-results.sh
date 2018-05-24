5e301c3ded7dc75bd7495c0d39be2073731652e2
Mark Syms <mark.syms@citrix.com>
2018-05-24 17:58:14 +0100

    CP-22685: add test results to results RPM
    Signed-off-by: Mark Syms <mark.syms@citrix.com>
    Reviewed-by: Chandrika Srinivasan <chandrika.srinivasan@citrix.com>

---
+++ collect-test-results.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+OUTPUT_DIR=$1
+
+if [ -z $OUTPUT_DIR ]; then
+    echo "Usage: $0 <output directory>"
+    exit 1
+fi
+
+mkdir -p $OUTPUT_DIR
+
+lcov --capture --directory . --rc lcov_branch_coverage=1 --no-external --output-file $OUTPUT_DIR/coverage.info
+genhtml $OUTPUT_DIR/coverage.info  --rc lcov_branch_coverage=1 --output-directory $OUTPUT_DIR/coverage-html
+tar cf $OUTPUT_DIR/test-results.tar `find mockatests -name \*.log`
