c4c6946f21b119876a65fd801698e009b7eeb505
Mark Syms <mark.syms@citrix.com>
2020-07-20 11:52:45 +0100

    CA-342553: don't try to read encryption key from raw parent
    Signed-off-by: Mark Syms <mark.syms@citrix.com>

--- vhd/lib/vhd-util-snapshot.c
+++ vhd/lib/vhd-util-snapshot.c
@@ -226,21 +226,23 @@ vhd_util_snapshot(int argc, char **argv)
 	if(err)
 		goto out;
 
-	/* Set keyhash if it exists in parent */
-	struct vhd_keyhash vhdhash;
-	err = vhd_open(&vhd, backing, VHD_OPEN_RDONLY);
-	if(err)
-		goto out;
-	err = vhd_get_keyhash(&vhd, &vhdhash);
-	vhd_close(&vhd);
-	if(err)
-		goto out;
-	if (vhdhash.cookie == 1){
-		err = vhd_open(&vhd, name, VHD_OPEN_RDWR);
+	if (!vhd_flag_test(flags, VHD_FLAG_CREAT_PARENT_RAW)) {
+		/* Set keyhash if it exists in parent */
+		struct vhd_keyhash vhdhash;
+		err = vhd_open(&vhd, backing, VHD_OPEN_RDONLY);
 		if(err)
 			goto out;
-		err = vhd_set_keyhash(&vhd, &vhdhash);
+		err = vhd_get_keyhash(&vhd, &vhdhash);
 		vhd_close(&vhd);
+		if(err)
+			goto out;
+		if (vhdhash.cookie == 1){
+			err = vhd_open(&vhd, name, VHD_OPEN_RDWR);
+			if(err)
+				goto out;
+			err = vhd_set_keyhash(&vhd, &vhdhash);
+			vhd_close(&vhd);
+		}
 	}
 
 out:
