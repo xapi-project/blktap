xapi-project/blktap

commit 65c6a3c567d08ff27ebec34ede69da308fdcbf4c

Remove inline from non-static functions
GCC reports:
tapdisk-vbd.h:249:13: error: inline function ‘tapdisk_vbd_contains_dead_rings’ declared but never defined [-Werror]
  249 | bool inline tapdisk_vbd_contains_dead_rings(td_vbd_t * vbd);
      |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

This effects:
timeval_to_us
tapdisk_vbd_contains_dead_rings
tapdisk_xenblkif_evtchn_event_id
tapdisk_xenblkif_chkrng_event_id
tapdisk_xenblkif_stoppolling_event_id

Remove the inline making to let the compiler decide how to handle them
and fix the error.

Signed-off-by: Jason Andryuk <jandryuk@gmail.com>

drivers/tapdisk-utils.c
@@ -440,7 +440,7 @@ out:
 
 const long long USEC_PER_SEC = 1000000L;
 
-inline long long timeval_to_us(struct timeval *tv)
+long long timeval_to_us(struct timeval *tv)
 {
 	return ((long long)tv->tv_sec * USEC_PER_SEC) + tv->tv_usec;
 }

drivers/tapdisk-utils.h
@@ -100,6 +100,6 @@ shm_create(struct shm *shm);
 int
 shm_destroy(struct shm *shm);
 
-inline long long timeval_to_us(struct timeval *tv);
+long long timeval_to_us(struct timeval *tv);
 
 #endif

drivers/tapdisk-vbd.c
@@ -1879,7 +1879,7 @@ tapdisk_vbd_stats(td_vbd_t *vbd, td_stats_t *st)
 }
 
 
-bool inline
+bool
 tapdisk_vbd_contains_dead_rings(td_vbd_t * vbd)
 {
     return !list_empty(&vbd->dead_rings);

drivers/tapdisk-vbd.h
@@ -246,5 +246,5 @@ void tapdisk_vbd_complete_block_status_request(td_request_t, int);
 /**
  * Tells whether the VBD contains at least one dead ring.
  */
-bool inline tapdisk_vbd_contains_dead_rings(td_vbd_t * vbd);
+bool tapdisk_vbd_contains_dead_rings(td_vbd_t * vbd);
 #endif

drivers/td-blkif.c
@@ -627,21 +627,21 @@ fail:
 }
 
 
-inline event_id_t
+event_id_t
 tapdisk_xenblkif_evtchn_event_id(const struct td_xenblkif *blkif)
 {
 	return blkif->ctx->ring_event;
 }
 
 
-inline event_id_t
+event_id_t
 tapdisk_xenblkif_chkrng_event_id(const struct td_xenblkif *blkif)
 {
 	return blkif->chkrng_event;
 }
 
 
-inline event_id_t
+event_id_t
 tapdisk_xenblkif_stoppolling_event_id(const struct td_xenblkif *blkif)
 {
 	return blkif->stoppolling_event;

drivers/td-blkif.h
@@ -270,21 +270,21 @@ tapdisk_xenblkif_find(const domid_t domid, const int devid);
  * channel can be shared by multiple block interfaces, the event ID will be
  * shared as well.
  */
-extern inline event_id_t
+extern event_id_t
 tapdisk_xenblkif_evtchn_event_id(const struct td_xenblkif *blkif);
 
 /**
  * Returns the event ID associated wit checking the ring. This is a private
  * event.
  */
-extern inline event_id_t
+extern event_id_t
 tapdisk_xenblkif_chkrng_event_id(const struct td_xenblkif * const blkif);
 
 /**
  * Returns the event ID associated with stopping polling. This is a private
  * event.
  */
-extern inline event_id_t
+extern event_id_t
 tapdisk_xenblkif_stoppolling_event_id(const struct td_xenblkif * const blkif);
 
 /**

commit ef2fa78a407346e3c88e5c60cbadb56a49cc5c41

tapdisk: Make timeval_to_us() static inline
commit 65c6a3c567d0 removed inline from timeval_to_us() to get blktap to
compile.  However, it can be made a proper static inline and avoid the
overhead of a call.

Fixes: 65c6a3c567d0 ("Remove inline from non-static functions")
Suggested-by: Andrew Cooper <andrew.cooper3@citrix.com>
Signed-off-by: Jason Andryuk <jandryuk@gmail.com>

drivers/tapdisk-utils.c
@@ -437,10 +437,3 @@ out:
     }
     return err;
 }
-
-const long long USEC_PER_SEC = 1000000L;
-
-long long timeval_to_us(struct timeval *tv)
-{
-	return ((long long)tv->tv_sec * USEC_PER_SEC) + tv->tv_usec;
-}

drivers/tapdisk-utils.h
@@ -100,6 +100,9 @@ shm_create(struct shm *shm);
 int
 shm_destroy(struct shm *shm);
 
-long long timeval_to_us(struct timeval *tv);
+static inline uint64_t timeval_to_us(struct timeval *tv)
+{
+	return (uint64_t)tv->tv_sec * 1000000 + tv->tv_usec;
+}
 
 #endif