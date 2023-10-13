8baa12db996d71f03a79493413d9dcec6e35c328
Jason Andryuk <jandryuk@gmail.com>
2023-10-13 09:04:39 -0400

    Fix -Werror=stringop-truncation errors
    In the lib/vhd code:
    In function 'safe_strncpy',
        inlined from 'vhd_initialize_footer' at libvhd.c:2882:2:
    include/util.h:46:17: error: 'strncpy' output truncated before terminating nul copying 3 bytes from a string of the same length [-Werror=stringop-truncation]
       46 |         pdest = strncpy(dest, src, n - 1);
    
    In the lvm code twice:
    In function 'safe_strncpy',
        inlined from 'lvm_copy_name' at lvm-util.c:71:2,
        inlined from 'lvm_scan_lvs' at lvm-util.c:289:9:
    include/util.h:46:17: error: 'strncpy' output may be truncated copying 255 bytes from a string of length 255 [-Werror=stringop-truncation]
       46 |         pdest = strncpy(dest, src, n - 1);
    
    For VHD, just set the footer bytes individually and avoid the string
    ops.
    
    For LVM, switching back to strncpy and providing the full destination
    size silences the errors.  strncpy does NUL terminate the string - it
    just may truncate.  (safe_strncpy silences many other instances of
    stringop-truncation like:
    "error: 'strncpy' specified bound 1024 equals destination size", so we
    want to keep it.)
    
    Signed-off-by: Jason Andryuk <jandryuk@gmail.com>

--- lvm/lvm-util.c
+++ lvm/lvm-util.c
@@ -68,7 +68,7 @@ lvm_copy_name(char *dst, const char *src, size_t size)
 	if (strnlen(src, size) == size)
 		return -ENAMETOOLONG;
 
-	safe_strncpy(dst, src, size);
+	strncpy(dst, src, size);
 	return 0;
 }
 
@@ -102,7 +102,7 @@ lvm_parse_pv(struct vg *vg, const char *name, int pvs, uint64_t start)
 	if (i == pvs)
 		return -ENOMEM;
 
-	err = lvm_copy_name(pv->name, name, sizeof(pv->name) - 1);
+	err = lvm_copy_name(pv->name, name, sizeof(pv->name));
 	if (err)
 		return err;
 
@@ -286,7 +286,7 @@ lvm_scan_lvs(struct vg *vg)
 		lv->segments      = segs;
 		lv->first_segment = seg;
 
-		err = lvm_copy_name(lv->name, name, sizeof(lv->name) - 1);
+		err = lvm_copy_name(lv->name, name, sizeof(lv->name));
 		if (err)
 			goto out;
 		err = -EINVAL;
