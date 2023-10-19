xapi-project/blktap

commit 6ffa1d8a979e3fb959baecc71ce34eab909fb716

CA-366761: replace use of strncpy with inlined wrapper
Signed-off-by: Mark Syms <mark.syms@citrix.com>

drivers/block-crypto.c
@@ -140,11 +140,11 @@ find_keyfile(char **keyfile, const char *dirs,
 		sep = strchr(dirs, ',');
 		/* get directory element */
 		if (sep == NULL) {
-			strncpy(keydir, dirs, sizeof(keydir));
+			safe_strncpy(keydir, dirs, sizeof(keydir));
 			dirs = NULL;
 		} else {
 			size_t len = sep - dirs;
-			strncpy(keydir, dirs, len);
+			safe_strncpy(keydir, dirs, len);
 			dirs = sep+1;
 		}
 

control/tap-ctl-ipc.c
@@ -46,6 +46,7 @@
 #include "tap-ctl.h"
 #include "blktap2.h"
 #include "compiler.h"
+#include "util.h"
 
 int tap_ctl_debug = 0;
 
@@ -253,8 +254,7 @@ tap_ctl_connect(const char *name, int *sfd)
 		return -ENAMETOOLONG;
 	}
 
-	strncpy(saddr.sun_path, name, sizeof(saddr.sun_path));
-	saddr.sun_path[sizeof(saddr.sun_path) - 1] = '\0';
+	safe_strncpy(saddr.sun_path, name, sizeof(saddr.sun_path));
 
 	err = connect(fd, (const struct sockaddr *)&saddr, sizeof(saddr));
 	if (err) {

control/tap-ctl-unpause.c
@@ -40,6 +40,7 @@
 #include <getopt.h>
 
 #include "tap-ctl.h"
+#include "util.h"
 
 int
 tap_ctl_unpause(const int id, const int minor, const char *params, int flags,
@@ -54,8 +55,8 @@ tap_ctl_unpause(const int id, const int minor, const char *params, int flags,
 	message.u.params.flags = flags;
 
 	if (params)
-		strncpy(message.u.params.path, params,
-				sizeof(message.u.params.path) - 1);
+		safe_strncpy(message.u.params.path, params,
+			     sizeof(message.u.params.path));
 	if (secondary) {
 		err = snprintf(message.u.params.secondary,
 				sizeof(message.u.params.secondary) - 1, "%s",

control/tap-ctl-xen.c
@@ -47,6 +47,7 @@
 
 #include "tap-ctl.h"
 #include "compiler.h"
+#include "util.h"
 
 int
 tap_ctl_connect_xenblkif(const pid_t pid, const domid_t domid, const int devid, int poll_duration,
@@ -75,8 +76,7 @@ tap_ctl_connect_xenblkif(const pid_t pid, const domid_t domid, const int devid, 
             EPRINTF("pool name too long: %s\n", pool);
             return -ENAMETOOLONG;
         }
-        strncpy(message.u.blkif.pool, pool, sizeof(message.u.blkif.pool));
-        message.u.blkif.pool[sizeof(message.u.blkif.pool) - 1] = '\0';
+        safe_strncpy(message.u.blkif.pool, pool, sizeof(message.u.blkif.pool));
     } else {
         message.u.blkif.pool[0] = 0;
     }

drivers/block-nbd.c
@@ -51,6 +51,7 @@
 #include "timeout-math.h"
 #include "tapdisk-nbdserver.h"
 #include "tapdisk-protocol-new.h"
+#include "util.h"
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -199,9 +200,8 @@ tdnbd_stash_passed_fd(int fd, char *msg, void *data)
 		close(passed_fds[free_index].fd);
 
 	passed_fds[free_index].fd = fd;
-	strncpy(passed_fds[free_index].id, msg,
-		sizeof(passed_fds[free_index].id) - 1);
-	passed_fds[free_index].id[sizeof(passed_fds[free_index].id) - 1] = '\0';
+	safe_strncpy(passed_fds[free_index].id, msg,
+		     sizeof(passed_fds[free_index].id));
 }
 
 static int
@@ -921,8 +921,7 @@ tdnbd_open(td_driver_t* driver, const char* name,
 			return -1;
 		}
 		prv->remote_un.sun_family = AF_UNIX;
-		strncpy(prv->remote_un.sun_path, name, sizeof(prv->remote_un.sun_path));
-		prv->remote_un.sun_path[sizeof(prv->remote_un.sun_path) - 1] = '\0';
+		safe_strncpy(prv->remote_un.sun_path, name, sizeof(prv->remote_un.sun_path));
 		len = strlen(prv->remote_un.sun_path)
 			+ sizeof(prv->remote_un.sun_family);
 		if ((rc = connect(prv->socket, (struct sockaddr*)&prv->remote_un, len)

drivers/block-valve.c
@@ -232,8 +232,7 @@ valve_sock_open(td_valve_t *valve)
 			err = -ENAMETOOLONG;
 			goto fail;
 		}
-		strncpy(addr.sun_path, valve->brname, sizeof(addr.sun_path));
-		addr.sun_path[sizeof(addr.sun_path) - 1] = '\0';
+		safe_strncpy(addr.sun_path, valve->brname, sizeof(addr.sun_path));
 	} else
 		snprintf(addr.sun_path, sizeof(addr.sun_path),
 			 "%s/%s", TD_VALVE_SOCKDIR, valve->brname);

drivers/tapdisk-control.c
@@ -61,6 +61,7 @@
 #include "tapdisk-nbdserver.h"
 #include "td-blkif.h"
 #include "timeout-math.h"
+#include "util.h"
 
 #define TD_CTL_MAX_CONNECTIONS  10
 #define TD_CTL_SOCK_BACKLOG     32
@@ -573,8 +574,8 @@ tapdisk_control_list(struct tapdisk_ctl_conn *conn,
 		response->u.list.path[0] = 0;
 
 		if (vbd->name)
-			strncpy(response->u.list.path, vbd->name,
-				sizeof(response->u.list.path));
+			safe_strncpy(response->u.list.path, vbd->name,
+				     sizeof(response->u.list.path));
 
 		tapdisk_control_write_message(conn, response);
 	}
@@ -1624,8 +1625,7 @@ tapdisk_control_create_socket(char **socket_path)
 		err = ENAMETOOLONG;
 		goto fail;
 	}
-	strncpy(saddr.sun_path, td_control.path, sizeof(saddr.sun_path));
-	saddr.sun_path[sizeof(saddr.sun_path) - 1] = '\0';
+	safe_strncpy(saddr.sun_path, td_control.path, sizeof(saddr.sun_path));
 
 	saddr.sun_family = AF_UNIX;
 

drivers/tapdisk-disktype.c
@@ -248,7 +248,7 @@ tapdisk_disktype_parse_params(const char *params, const char **_path)
 		return -ENAMETOOLONG;
 
 	memset(name, 0, sizeof(name));
-	strncpy(name, params, len);
+	safe_strncpy(name, params, len);
 
 	type = tapdisk_disktype_find(name);
 

commit f744dfc1abc7a5791eb416b638cdd4af0f155e9f

block-crypto: Fix OPEN_XT crypto build
We need to include the util.h for the safe_strncpy declararion.

Fixes: 6ffa1d8a97 "CA-366761: replace use of strncpy with inlined wrapper"
Signed-off-by: Jason Andryuk <jandryuk@gmail.com>

drivers/block-crypto.c
@@ -42,6 +42,7 @@
 #include "libvhd.h"
 #include "tapdisk.h"
 #include "vhd-util.h"
+#include "util.h"
 
 #include "crypto/compat-crypto-openssl.h"
 #include "crypto/xts_aes.h"