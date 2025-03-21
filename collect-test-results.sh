xapi-project/blktap

commit 8c5bb7da8b67ba36d47ed8a08adedf439f7a46d7

CA-387574: Fix build time coverage gathering
Directing the profile-dir to an out of tree location results in gcc 12
creating paths with the separators of the absolute source path
converted to # characters. When subsequently using gcov or lcov to
analyse the coverage this produces errors and no coverage data is
produced. As the collect-test-results.sh pulls these files back into
the build tree and published them from there it isn't necessary to
write them elsewhere and this avoids the decorated paths being created.

Signed-off-by: Mark Syms <mark.syms@cloud.com>

collect-test-results.sh
@@ -11,16 +11,9 @@ fi
 
 mkdir -p $OUTPUT_DIR
 
-find . -name \*.gcda -exec rm {} \;
-
-lcov --capture --initial --directory . --rc lcov_branch_coverage=1 --no-external --output-file $OUTPUT_DIR/coverage_base.info
-
-(cd /tmp/coverage/blktap; tar cf - `find . -name \*.gcda`) | tar xf -
-
-lcov --capture --directory . --rc lcov_branch_coverage=1 --no-external --output-file $OUTPUT_DIR/coverage_test.info
-lcov --rc lcov_branch_coverage=1 --add-tracefile $OUTPUT_DIR/coverage_base.info --add-tracefile $OUTPUT_DIR/coverage_test.info --output-file $OUTPUT_DIR/coverage.info
+lcov --capture --directory . --rc lcov_branch_coverage=1 --no-external --output-file $OUTPUT_DIR/coverage.info
 genhtml $OUTPUT_DIR/coverage.info  --rc lcov_branch_coverage=1 --output-directory $OUTPUT_DIR/coverage-html
 tar cf $OUTPUT_DIR/test-results.tar `find mockatests -name \*.log`
 tar cf $OUTPUT_DIR/gcov-files.tar `find . -name \*.gcda -or -name \*.gcno`
 
-lcov -l $OUTPUT_DIR/coverage.info
+lcov --rc lcov_branch_coverage=1 -l $OUTPUT_DIR/coverage.info

cbt/Makefile.am
@@ -1,6 +1,6 @@
 AM_CFLAGS  = -Wall
 AM_CFLAGS += -Werror
-AM_CFLAGS += $(if $(GCOV),-fprofile-dir=/tmp/coverage/blktap/cbt -fprofile-arcs -ftest-coverage)
+AM_CFLAGS += $(if $(GCOV),-fprofile-arcs -ftest-coverage)
 
 AM_CPPFLAGS = -I$(top_srcdir)/include
 

control/Makefile.am
@@ -1,7 +1,7 @@
 
 AM_CFLAGS  = -Wall
 AM_CFLAGS += -Werror
-AM_CFLAGS += $(if $(GCOV),-fprofile-dir=/tmp/coverage/blktap/control -fprofile-arcs -ftest-coverage)
+AM_CFLAGS += $(if $(GCOV),-fprofile-arcs -ftest-coverage)
 AM_CFLAGS += -g
 
 AM_CPPFLAGS  = -D_GNU_SOURCE

cpumond/Makefile.am
@@ -1,7 +1,7 @@
 
 AM_CFLAGS  = -Wall
 AM_CFLAGS += -Werror
-AM_CFLAGS += $(if $(GCOV),-fprofile-dir=/tmp/coverage/blktap/cpumond -fprofile-arcs -ftest-coverage)
+AM_CFLAGS += $(if $(GCOV),-fprofile-arcs -ftest-coverage)
 
 AM_CPPFLAGS  = -D_GNU_SOURCE
 AM_CPPFLAGS += -I$(top_srcdir)/include

drivers/Makefile.am
@@ -1,7 +1,7 @@
 SUBDIRS = crypto
 AM_CFLAGS  = -Wall
 AM_CFLAGS += -Werror
-AM_CFLAGS += $(if $(GCOV),-fprofile-dir=/tmp/coverage/blktap/drivers -fprofile-arcs -ftest-coverage)
+AM_CFLAGS += $(if $(GCOV),-fprofile-arcs -ftest-coverage)
 # TODO add -Wextra
 
 AM_CPPFLAGS  = -D_GNU_SOURCE

lvm/Makefile.am
@@ -1,7 +1,7 @@
 
 AM_CFLAGS  = -Wall
 AM_CFLAGS += -Werror
-AM_CFLAGS += $(if $(GCOV),-fprofile-dir=/tmp/coverage/blktap/lvm -fprofile-arcs -ftest-coverage)
+AM_CFLAGS += $(if $(GCOV),-fprofile-arcs -ftest-coverage)
 
 sbin_PROGRAMS = lvm-util
 

mockatests/cbt/Makefile.am
@@ -1,6 +1,6 @@
 AM_CFLAGS  = -Wall
 AM_CFLAGS += -Werror
-AM_CFLAGS += -fprofile-dir=/tmp/coverage/blktap/mockatests/cbt -fprofile-arcs -ftest-coverage
+AM_CFLAGS += -fprofile-arcs -ftest-coverage
 AM_CFLAGS += -Og -fno-inline-functions -g
 
 AM_CPPFLAGS = -I$(top_srcdir)/include -I$(top_srcdir)/cbt -I../include

mockatests/control/Makefile.am
@@ -1,6 +1,6 @@
 AM_CFLAGS  = -Wall
 AM_CFLAGS += -Werror
-AM_CFLAGS += -fprofile-dir=/tmp/coverage/blktap/mockatests/control -fprofile-arcs -ftest-coverage
+AM_CFLAGS += -fprofile-arcs -ftest-coverage
 AM_CFLAGS += -Og -fno-inline -g
 
 AM_CPPFLAGS = -I$(top_srcdir)/include -I$(top_srcdir)/control -I../include

commit 3e61ff5e178aa5aacf8456e5f86edce10ec67e49

CA-387574: Get coverage for all files
The previous change 8c5bb7d, inadvertently omitted a number of 0%
coverage files, presenting a misleading report of coverage. Rectify
that by calling lcov with a --initial.

Signed-off-by: Mark Syms <mark.syms@cloud.com>

collect-test-results.sh
@@ -11,7 +11,9 @@ fi
 
 mkdir -p $OUTPUT_DIR
 
-lcov --capture --directory . --rc lcov_branch_coverage=1 --no-external --output-file $OUTPUT_DIR/coverage.info
+lcov --capture --initial --directory . --rc lcov_branch_coverage=1 --no-external --output-file $OUTPUT_DIR/coverage_base.info
+lcov --capture --directory . --rc lcov_branch_coverage=1 --no-external --output-file $OUTPUT_DIR/coverage_test.info
+lcov --rc lcov_branch_coverage=1 --add-tracefile $OUTPUT_DIR/coverage_base.info --add-tracefile $OUTPUT_DIR/coverage_test.info --output-file $OUTPUT_DIR/coverage.info
 genhtml $OUTPUT_DIR/coverage.info  --rc lcov_branch_coverage=1 --output-directory $OUTPUT_DIR/coverage-html
 tar cf $OUTPUT_DIR/test-results.tar `find mockatests -name \*.log`
 tar cf $OUTPUT_DIR/gcov-files.tar `find . -name \*.gcda -or -name \*.gcno`