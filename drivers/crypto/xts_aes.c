469f9ac9216cc7eac65037da95cc29cc5d825d32
Anthoine Bourgeois <anthoine.bourgeois@vates.tech>
2026-09-02 20:13:28 +0200

    LICENSE: update copyright holder and license accordingly
    I also update dates of the copyright to the latest year found
    in the history.
    
    Signed-off-by: Anthoine Bourgeois <anthoine.bourgeois@vates.tech>

--- drivers/crypto/xts_aes.c
+++ drivers/crypto/xts_aes.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2010, XenSource Inc.
+ * Copyright (c) 2019, Citrix Systems, Inc.
+ *
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -9,9 +10,9 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of XenSource Inc. nor the names of its contributors
- *       may be used to endorse or promote products derived from this software
- *       without specific prior written permission.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -25,11 +26,6 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-/*
- * Copyright (c) 2014 Citrix Systems, Inc.
- */
-
 
 #include <err.h>
 #include <stdio.h>
